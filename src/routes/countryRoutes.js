85aecad0b86de20b106d33e4c8db202799c97f0d
Andresuito <barrancorios@gmail.com>
2024-05-08 00:02:01 +0200

    chore: Update countryRoutes.js to include requiresToken middleware for /all route

--- src/routes/countryRoutes.js
+++ src/routes/countryRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const requiresToken = require("../middleware/requiresToken");
 const countryController = require("../controllers/countryController");
 
-router.get("/all", countryController.getAllCountries);
+router.get("/all", requiresToken, countryController.getAllCountries);
 router.get("/:name", countryController.getCountryByName);
 
 module.exports = router;
