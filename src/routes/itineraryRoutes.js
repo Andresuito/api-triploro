8df788e7f792a920c39a40e0c65dd9e74765a8bf
Andresuito <barrancorios@gmail.com>
2024-05-19 19:06:38 +0200

    feat: Add favorite itinerary functionality and route

--- src/routes/itineraryRoutes.js
+++ src/routes/itineraryRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const requiresToken = require("../middleware/requiresToken");
 const itineraryController = require("../controllers/itineraryController");
 
+router.get("/allPublic", itineraryController.getAllItinerariesPublic);
 router.post("/create", requiresToken, itineraryController.createItinerary);
 router.get("/user/:userId", requiresToken, itineraryController.getAllUserItineraries);
 router.get("/:code", requiresToken, itineraryController.getItineraryByCode);
 router.patch("/update/image/:code", requiresToken, itineraryController.updateItineraryImage);
+router.post("/favorite/:code", requiresToken, itineraryController.favoriteItinerary);
 
 module.exports = router;
