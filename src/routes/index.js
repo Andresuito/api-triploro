3c5952ddd5e995281d3e90d625a66e6745b86641
Andresuito <barrancorios@gmail.com>
2024-06-03 16:15:58 +0200

    feat: Add activity routes and update index.js
    This commit adds the activity routes to the router in the index.js file. It also includes the necessary import statement for the activityRoutes module. This change allows the application to handle requests related to activities.
    
    Recent user commits:
    - feat: Add activity routes
    - feat: Add Activity model with associations to Itinerary
    - refactor: Update activityController to follow consistent coding style
    - feat: Add delete itinerary route
    - feat: Add latitude and longitude fields to Itinerary model
    - feat: Add delete itinerary functionality
    - refactor: Update createFakeItineraries function to generate random destinations for itineraries
    - feat: Add 'rimraf' npm dependency for cleaning build artifacts
    - refactor: Update createFakeItineraries function to generate 100 itineraries instead of 20
    - refactor: Add unique constraint to 'code' field in Itinerary model

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const countryRoutes = require("./countryRoutes");
 const destinationRoutes = require("./destinationRoutes");
 const itineraryRoutes = require("./itineraryRoutes");
 const friendshipRoutes = require("./friendshipRoutes");
+const activityRoutes = require("./activityRoutes");
 
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Status Ok" });
@@ -18,5 +19,6 @@ router.use("/country", countryRoutes);
 router.use("/destination", destinationRoutes);
 router.use("/itinerary", itineraryRoutes);
 router.use("/friendship", friendshipRoutes);
+router.use("/activity", activityRoutes);
 
 module.exports = router;
