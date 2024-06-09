75f9db8c7f6f10a4706c13d606cae7a193b9d81b
Andresuito <barrancorios@gmail.com>
2024-06-09 14:11:47 +0200

    feat: Remove console.log statements from invitationController

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const sequelize = require("./config/sequelizeConfig");
-const addUsers = require("./scripts/addFakeUsers.js");
 const addCountries = require("./scripts/addCountries");
 const addCities = require("./scripts/addCities");
-const addTags = require("./scripts/addTags");
-const addTagsToDestinations = require("./scripts/addTagsToDestinations.js");
-const createFakeItineraries = require("./scripts/addItineraries.js");
 
-// Importar modelos
 const User = require("./src/models/User");
 const Itinerary = require("./src/models/Itinerary");
 const Invitation = require("./src/models/Invitation");
@@ -21,6 +16,8 @@ const Destination = require("./src/models/Destinations");
 const FavoriteItinerary = require("./src/models/FavoriteItinerary");
 const PersonalItinerary = require("./src/models/PersonalItinerary");
 const Tag = require("./src/models/Tag");
+
+const setAssociations = require("./src/models/association");
 
 const {
   errorHandler,
@@ -49,41 +46,14 @@ app.use("/img", express.static("img"));
 app.use("/uploads", express.static("uploads"));
 app.use("/api/v1", require("./src/routes"));
 
-User.hasMany(Invitation, { foreignKey: "userId", as: "invitations" });
-Invitation.belongsTo(User, { foreignKey: "userId", as: "inviter" });
-
-Itinerary.hasMany(Invitation, { foreignKey: "itineraryId", as: "invitations" });
-Invitation.belongsTo(Itinerary, { foreignKey: "itineraryId", as: "itinerary" });
-
-Itinerary.belongsToMany(Tag, { through: "ItineraryTag" });
-Tag.belongsToMany(Itinerary, { through: "ItineraryTag" });
-
-Itinerary.hasMany(FavoriteItinerary, {
-  foreignKey: "code",
-  sourceKey: "code",
-  as: "favoriteItineraries",
-});
-FavoriteItinerary.belongsTo(Itinerary, {
-  foreignKey: "code",
-  targetKey: "code",
-  as: "itinerary",
-});
-
-Itinerary.hasMany(PersonalItinerary, {
-  foreignKey: "itineraryId",
-  as: "personalItineraries",
-});
-PersonalItinerary.belongsTo(Itinerary, { foreignKey: "itineraryId" });
-
-User.belongsToMany(User, {
-  as: "friends",
-  through: "Friendship",
-  foreignKey: "userId",
-  otherKey: "friendId",
-});
-
-User.hasMany(FavoriteItinerary, { foreignKey: "userId" });
-User.hasMany(PersonalItinerary, { foreignKey: "userId" });
+setAssociations(
+  User,
+  Invitation,
+  Itinerary,
+  Tag,
+  FavoriteItinerary,
+  PersonalItinerary
+);
 
 app.use(errorHandler);
 app.use(notFoundHandler);
@@ -91,15 +61,11 @@ app.use(notFoundHandler);
 const port = process.env.PORT || 8000;
 
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(() => {
     console.log("Model synchronized with the database");
-    addUsers();
     addCountries();
     setTimeout(addCities, 2000);
-    setTimeout(addTags, 2000);
-    setTimeout(addTagsToDestinations, 3000);
-    setTimeout(createFakeItineraries, 3000);
     app.listen(port, () => {
       console.log(`Express server running at http://localhost:${port}/api/v1/`);
     });
---
+++ src/models/association.js
@@ -0,0 +1,50 @@
+module.exports = function (
+  User,
+  Invitation,
+  Itinerary,
+  Tag,
+  FavoriteItinerary,
+  PersonalItinerary
+) {
+  User.hasMany(Invitation, { foreignKey: "userId", as: "invitations" });
+  Invitation.belongsTo(User, { foreignKey: "userId", as: "inviter" });
+
+  Itinerary.hasMany(Invitation, {
+    foreignKey: "itineraryId",
+    as: "invitations",
+  });
+  Invitation.belongsTo(Itinerary, {
+    foreignKey: "itineraryId",
+    as: "itinerary",
+  });
+
+  Itinerary.belongsToMany(Tag, { through: "ItineraryTag" });
+  Tag.belongsToMany(Itinerary, { through: "ItineraryTag" });
+
+  Itinerary.hasMany(FavoriteItinerary, {
+    foreignKey: "code",
+    sourceKey: "code",
+    as: "favoriteItineraries",
+  });
+  FavoriteItinerary.belongsTo(Itinerary, {
+    foreignKey: "code",
+    targetKey: "code",
+    as: "itinerary",
+  });
+
+  Itinerary.hasMany(PersonalItinerary, {
+    foreignKey: "itineraryId",
+    as: "personalItineraries",
+  });
+  PersonalItinerary.belongsTo(Itinerary, { foreignKey: "itineraryId" });
+
+  User.belongsToMany(User, {
+    as: "friends",
+    through: "Friendship",
+    foreignKey: "userId",
+    otherKey: "friendId",
+  });
+
+  User.hasMany(FavoriteItinerary, { foreignKey: "userId" });
+  User.hasMany(PersonalItinerary, { foreignKey: "userId" });
+};
