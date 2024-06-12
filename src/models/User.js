4d679be4a222f05db645d70e0dc92285d616fbf6
Andresuito <barrancorios@gmail.com>
2024-06-12 14:54:07 +0200

    refactor: Remove unused fields from User model

--- src/models/User.js
+++ src/models/User.js
@@ -27,14 +27,6 @@ const User = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    receiveNewsletter: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-    receiveNewDestination: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
     isActivated: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
